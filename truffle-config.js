53e9d955d2c57daa23317f1b8ef8c69642727092
mininjin <mininjin332@gmail.com>
2022-08-20 21:45:51 +0900

    add truffle config

---
+++ truffle-config.js
@@ -0,0 +1,22 @@
+/**
+ * See https://trufflesuite.com/docs/truffle/reference/configuration fo details
+ */
+
+require("ts-node/register");
+
+module.exports = {
+  // directory configs
+  contracts_directory: "./contracts",
+  contracts_build_directory: "./build/contracts",
+  test_directory: "./build/test",
+  migrations_directory: "./build/migrations",
+  // netwrok config
+  networks: {
+    // local server by Ganache
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+  },
+};
